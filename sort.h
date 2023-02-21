4b9f8993286c6d0394284324d66af406d5cfeca1
Michael Aloh <mykelaloh@gmail.com>
2023-02-20 17:21:47 -0800

    done

--- sort.h
+++ sort.h
@@ -20,4 +20,7 @@ void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 void swap(int *a, int *b);
 void bubble_sort(int *array, size_t size);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
 #endif/*SORT_H*/
